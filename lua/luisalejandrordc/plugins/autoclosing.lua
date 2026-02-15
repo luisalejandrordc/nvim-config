a661851d0cf515d9e71c7e1857993f91011159e1
Luis Alejandro <luisalejandrordc@gmail.com>
2026-02-14 23:03:45 -0500

    Remove autoclosing.lua plugin file

--- lua/luisalejandrordc/plugins/autoclosing.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "windwp/nvim-ts-autotag",
-  ft = { "html", "javascript", "typescript", "tsx", "jsx", "vue", "svelte", "xml" },
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-}
-
