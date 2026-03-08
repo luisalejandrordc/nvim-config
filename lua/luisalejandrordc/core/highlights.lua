0cbc2868bfd9679bb9ea52b2a2f7f46ebe4704c4
Luis Alejandro <luisalejandrordc@gmail.com>
2026-03-08 01:05:47 -0500

    Make changes

--- lua/luisalejandrordc/core/highlights.lua
+++ lua/luisalejandrordc/core/highlights.lua
@@ -12,11 +12,14 @@ function M.transparent_nvim_tree()
 	vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { fg = "#6E7276" })
 	vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#9CA0A4" })
 
+	-- vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#343454" })
+
 	-- Enable current line highlight
 	vim.opt.cursorline = true
 	-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2B2B2B" }) -- slightly lighter than background
-	-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" }) -- slightly lighter than background
 	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1A1A1A" }) -- slightly lighter than background
+	-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2A2A3C" }) -- slightly lighter than background
+	-- vim.api.nvim_set_hl(0, "LineNr", { fg = "#5A5A78", bg = "#1E1E2E" })
 	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#47FF9C", bold = true }) -- optional, line number highlight
 end
 
--- lua/luisalejandrordc/plugins/monokai.lua
+++ lua/luisalejandrordc/plugins/monokai.lua
@@ -6,6 +6,7 @@ return {
 		require("monokai-pro").setup({
 			filter = "spectrum", -- classic | octagon | pro | machine | spectrum | ristretto
 			transparent_background = true,
+			terminal_colors = false,
 
 			styles = {
 				keyword = { italic = false },
@@ -14,6 +15,20 @@ return {
 		})
 
 		vim.cmd.colorscheme("monokai-pro")
+
+		-- Change editor background
+		-- vim.api.nvim_set_hl(0, "Normal", { bg = "#222222" })
+		-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "#222222" })
+		-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#222222" })
+		-- vim.api.nvim_set_hl(0, "LineNr", { bg = "#222222" })
+		-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#222222" })
+
+		-- vim.api.nvim_set_hl(0, "Normal", { bg = "#1E1E2E" })
+		-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1E1E2E" })
+		-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#1E1E2E" })
+		-- vim.api.nvim_set_hl(0, "LineNr", { bg = "#1E1E2E" })
+		-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#1E1E2E" })
+		-- vim.api.nvim_set_hl(0, "Visual", { bg = "#3E3E5E" })
 
 		-- Set LaTeX colors manually
 		vim.api.nvim_set_hl(0, "@label.latex", { fg = "#FC9867" })
