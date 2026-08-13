7c91266b9f0b9bcb7bd000a9d3c07edb2fa39e19
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-13 11:41:35 -0500

    update treesitter.lua (switched to main branch), and telescope.lua (switched to master branch)

--- lua/luisalejandrordc/plugins/treesitter.lua
+++ lua/luisalejandrordc/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
+
 	dependencies = {
 		{
 			"windwp/nvim-ts-autotag",
@@ -11,59 +12,65 @@ return {
 			end,
 		},
 	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "latex" },
-			},
-			-- enable indentation
-			indent = { enable = true, disable = { "cpp" } },
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"python",
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c",
-				"cpp",
-				"php",
-				"java",
-				"sql",
-				"latex",
-				"arduino",
-				"go",
-				"r",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+	config = function()
+		require("nvim-treesitter").setup()
+
+		local ensure_installed = {
+			"python",
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"cpp",
+			"php",
+			"java",
+			"sql",
+			"latex",
+			"arduino",
+			"go",
+			"r",
+		}
+		require("nvim-treesitter").install(ensure_installed)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local filetype = vim.bo[args.buf].filetype
+
+				if vim.bo[args.buf].buftype ~= "" or filetype == "" then
+					return
+				end
+
+				local lang = vim.treesitter.language.get_lang(filetype)
+
+				if not lang then
+					return
+				end
+
+				if not pcall(vim.treesitter.start, args.buf, lang) then
+					return
+				end
+
+				if filetype ~= "cpp" then
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
 }
