0426f1fda6076f951833651b736234aed90b4e8d
luisalejandrordc <luisalejandrordc@gmail.com>
2026-09-05 14:05:59 -0500

    configure definition float border on hovering + K

--- lua/luisalejandrordc/core/highlights.lua
+++ lua/luisalejandrordc/core/highlights.lua
@@ -15,18 +15,20 @@ function M.transparent_nvim_tree()
 	-- vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#2C2C2C" })
 
 	vim.api.nvim_set_hl(0, "NormalFloat", {
-		-- bg = "#191724",
-		-- bg = "#121212",
-		bg = "none",
+		bg = "NONE",
+		fg = "#E0DEF4",
+	})
+
+	vim.api.nvim_set_hl(0, "FloatBorder", {
+		bg = "NONE",
 		fg = "#E0DEF4",
 	})
 
 	-- Enable current line highlight
 	vim.opt.cursorline = true
-	-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#181818" }) -- slightly lighter than background
-	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#121212" }) -- slightly lighter than background
+	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#121212" })
 	vim.api.nvim_set_hl(0, "LineNr", { fg = "#5A5A78", bg = "NONE" })
-	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#E0DEF4", bold = true }) -- optional, line number highlight
+	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#E0DEF4", bold = true })
 end
 
 return M
--- lua/luisalejandrordc/plugins/lsp/lspconfig.lua
+++ lua/luisalejandrordc/plugins/lsp/lspconfig.lua
@@ -67,7 +67,9 @@ return {
 				end, opts)
 
 				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+				keymap.set("n", "K", function()
+					vim.lsp.buf.hover({ border = "rounded" })
+				end, opts)
 
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rs", ":lsp restart<CR>", opts) -- mapping to restart lsp if necessary
