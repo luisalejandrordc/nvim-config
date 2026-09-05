522c6d4a58732ad6f731251eecd4219c2a11eafc
luisalejandrordc <luisalejandrordc@gmail.com>
2026-09-04 19:05:07 -0500

    add smear-cursor plugin

---
+++ lua/luisalejandrordc/plugins/smear-cursor.lua
@@ -0,0 +1,22 @@
+return {
+	"sphamba/smear-cursor.nvim",
+
+	lazy = false,
+
+	opts = {
+		stiffness = 0.8,
+		cursor_color = "#47FF9C",
+		smear_between_buffers = true,
+		smear_between_neighbor_lines = true,
+		scroll_buffer_space = true,
+		smear_insert_mode = true,
+	},
+
+	keys = {
+		{
+			"<leader>sc",
+			"<cmd>SmearCursorToggle<cr>",
+			desc = "Toggle smear cursor",
+		},
+	},
+}
