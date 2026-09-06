d65c9c3bfbd0094d3039cac410342b5aa1f25b09
luisalejandrordc <luisalejandrordc@gmail.com>
2026-09-05 22:21:28 -0500

    update cursor bg color

--- lua/luisalejandrordc/core/highlights.lua
+++ lua/luisalejandrordc/core/highlights.lua
@@ -7,10 +7,10 @@ function M.transparent_nvim_tree()
 	vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "NONE", fg = "NONE" })
 	vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { bg = "NONE", fg = "NONE" })
 
-	vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = "#A6ADB4" })
-	vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = "#D1D7DD", bold = true })
-	vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { fg = "#70777E" })
-	vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#A6ADB4" })
+	vim.api.nvim_set_hl(0, "nvimtreefoldername", { fg = "#a6adb4" })
+	vim.api.nvim_set_hl(0, "nvimtreeopenedfoldername", { fg = "#d1d7dd", bold = true })
+	vim.api.nvim_set_hl(0, "nvimtreeemptyfoldername", { fg = "#70777e" })
+	vim.api.nvim_set_hl(0, "nvimtreefoldericon", { fg = "#a6adb4" })
 
 	-- vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#2C2C2C" })
 
@@ -22,7 +22,7 @@ function M.transparent_nvim_tree()
 	vim.opt.cursorline = true
 	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#121212" })
 	vim.api.nvim_set_hl(0, "LineNr", { fg = "#5A5A78", bg = "NONE" })
-	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFFFFF", bold = true })
+	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e5b567", bold = true })
 end
 
 return M
